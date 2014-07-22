c2f95c93a94fe88372caa737ea092515d01d3aae
onlyutkarsh <onlyutkarsh@gmail.com>
2014-07-22 19:17:54 +0100

    Initial Check-in

---
+++ DropDownButton.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace DropDownLinkButton
+{
+    public class DropDownButton : ToggleButton
+    {
+        // *** Dependency Properties ***
+
+
+        public static readonly DependencyProperty DropDownProperty = DependencyProperty.Register("DropDown", typeof(ContextMenu), typeof(DropDownButton), new UIPropertyMetadata(null));
+
+        // *** Constructors *** 
+
+        public DropDownButton()
+        {
+            // Bind the ToogleButton.IsChecked property to the drop-down's IsOpen property 
+
+            Binding binding = new Binding("DropDown.IsOpen");
+            binding.Source = this;
+            this.SetBinding(IsCheckedProperty, binding);
+        }
+
+        // *** Properties *** 
+
+        public ContextMenu DropDown
+        {
+            get
+            {
+                return (ContextMenu)GetValue(DropDownProperty);
+            }
+            set
+            {
+                SetValue(DropDownProperty, value);
+            }
+        }
+
+        // *** Overridden Methods *** 
+
+        protected override void OnClick()
+        {
+            if (DropDown != null)
+            {
+                // If there is a drop-down assigned to this button, then position and display it 
+
+                DropDown.PlacementTarget = this;
+                DropDown.Placement = PlacementMode.Bottom;
+
+                DropDown.IsOpen = true;
+            }
+        }
+    }
+}
